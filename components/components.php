f99267fc23775c6d77f1dff263ca18bb8bf05de0
nhdzzdna <nhdzzdna@gmail.com>
2025-11-26 12:28:12 +0700

    tambah berita

--- components/components.php
+++ components/components.php
@@ -9,16 +9,35 @@ function head($title){ ?>
     <link rel="stylesheet" href="../style/components.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Marcellus+SC&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Marcellus+SC&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 <?php } ?>
 
 <?php
 function footer(){ ?>
     <div class="footer">
         <div class="isiFooter">
-
+            <div class="isiFooter1">
+                <a href="../pages/landingPage.php" class="logoFooter">
+                    <img src="../assets/components image/logoFooter.svg" alt="Ikon Desa">
+                    <div>
+                        <h3>Desa Panghuripan</h3>
+                        <p>Kabupaten Sleman</p>
+                    </div>
+                </a>
+                <div class="sloganFooter"><p>“Nguripi, Nglestari, Ngluhuraké”</p></div>
+                <div class="alamatFooter">Jl. Dalgino, Area Sawah, Banyuraden, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55293</div>
+            </div>
+            <div class="isiFooter2"></div>
+            <div class="isiFooter3"></div>
+            <div class="isiFooter4">
+                <div></div>
+                <div></div>
+            </div>
         </div>
-        <div class="copyright"></div>
+        <div class="copyright">
+            <hr>
+            <p>© 2025 Desa Panghuripan. All Right Reserved</p>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <?php
@@ -30,7 +49,7 @@ function footer(){ ?>
 function navbar(){ ?>
     <nav>
         <a href="../pages/landingPage.php" class="logo">
-            <img src="../assets/navbar/logo.svg" alt="Ikon Desa">
+            <img src="../assets/components image/logo.svg" alt="Ikon Desa">
             <div>
                 <h3>Desa Panghuripan</h3>
                 <p>Kabupaten Sleman</p>
@@ -46,7 +65,7 @@ function navbar(){ ?>
                 <div class="pilihMenu"></div>
             </div>
             <div class="layananWarga" >
-                <ahref="../pages/layananWarga.php">Layanan Warga</ahref=> 
+                <a href="../pages/layananWarga.php">Layanan Warga</a> 
             </div>
             
         </div>
